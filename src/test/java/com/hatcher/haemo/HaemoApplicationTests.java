2897af6382bb97d603eba77d669134ef50eebecb
joonghyun <joongh@ewhain.net>
2024-03-13 09:56:57 +0900

    #1 infra: Initialize the project

---
+++ src/test/java/com/hatcher/haemo/HaemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hatcher.haemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
