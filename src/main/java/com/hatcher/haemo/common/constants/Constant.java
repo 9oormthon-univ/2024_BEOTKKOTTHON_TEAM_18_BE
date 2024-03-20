44d6284db26361de4e22036e8c9206437cdaa669
joonghyun <joongh@ewhain.net>
2024-03-21 02:28:21 +0900

    #5 feat: Constant 생성

---
+++ src/main/java/com/hatcher/haemo/common/constants/Constant.java
@@ -0,0 +1,13 @@
+package com.hatcher.haemo.common.constants;
+
+public class Constant {
+    public static final String ACTIVE = "active";
+    public static final String INACTIVE = "inactive"; // 탈퇴
+    public static final String LOGOUT = "logout"; // 로그아웃
+
+    public static class Recruitment {
+        public static final String RECRUITING = "recruiting"; // 모집중
+        public static final String DONE = "done"; // 모집완료
+        public static final String CANCELLED = "cancelled"; // 모집취소
+    }
+}
